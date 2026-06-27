63a4997087aac4139405f7615adbd3c7b7110d25
onlineregistration1 <kibetriskid@gmail.com>
2026-06-27 22:29:56 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,30 @@
 
+// Firebase SDK Imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDh2fHMUbysckAxdIA8dcz8sHWrcLbW1EQ",
+  authDomain: "african-enterprise-challenge-f.firebaseapp.com",
+  projectId: "african-enterprise-challenge-f",
+  storageBucket: "african-enterprise-challenge-f.firebasestorage.app",
+  messagingSenderId: "85146331644",
+  appId: "1:85146331644:web:b28599e0c8b75498fe6a79"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Services
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// Export for other pages
+export { app, auth, db };
