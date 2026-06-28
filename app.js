1d538c8df9a008f33cfe43743c4bb59af4fe8ffb
onlineregistration1 <kibetriskid@gmail.com>
2026-06-28 16:34:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -96,6 +96,7 @@ $('sendBtn').onclick = async ()=>{
   if(!txt) return;
   await sendMsg(txt,'');
   $('msgInput').value=''; $('previewBar').classList.remove('show');
+  setTimeout(()=>$('msgInput').focus(),0); // Keep keyboard open
 }
 
 $('imgInput').onchange = async (e)=>{
@@ -106,6 +107,7 @@ $('imgInput').onchange = async (e)=>{
   const url=await getDownloadURL(snap.ref);
   await sendMsg('',url);
   e.target.value='';
+  setTimeout(()=>$('msgInput').focus(),0); // Keep keyboard open
 }
 
 $('msgInput').oninput=()=>{
